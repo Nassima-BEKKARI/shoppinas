8c5f788569ab7a64faa750ba04c83cc5f931e03f
nasalia <n.bekkari1987@gmail.com>
2022-07-06 22:47:19 +0200

    user

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -6,6 +6,7 @@ use DateTime;
 use App\Entity\Produit;
 use App\Form\ProduitType;
 use App\Repository\ProduitRepository;
+use App\Repository\CategorieRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,12 +26,29 @@ class ProduitController extends AbstractController
         ]);
     }
 
-    public function allProduits(ProduitRepository $repo)
+    public function allProduits(ProduitRepository $repo, CategorieRepository $repoCat)
     {
         $produits = $repo->findAll();
+        $categories = $repoCat->findAll();
 
         return $this->render('produit/produits.html.twig', [
-            'produits'=>$produits
+            'produits'=>$produits,
+            'categories'=>$categories
+        ]);
+    }
+
+    public function categorieProduits($id, CategorieRepository $repo)
+    {
+        //On récupère la catégorie sur laquelle on a cliqué pour accéder aux produits liés
+        $categorie = $repo->find($id);
+        $produits = $categorie->getProduits();
+
+        //On récupère toutes les catégories pour les afficher dans la liste de la page
+        $categories = $repo->findAll();
+
+        return $this->render('produit/produits.html.twig',[
+            'produits' => $produits,
+            'categories' => $categories
         ]);
     }
 }
