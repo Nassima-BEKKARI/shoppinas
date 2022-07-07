7c74133989e7b0c0362425ec26d716fe91c5a5d5
nasalia <n.bekkari1987@gmail.com>
2022-07-07 14:19:31 +0200

    user et autorisations

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\AdminType;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -27,5 +30,37 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    public function passerAdmin(UserRepository $repo, Request $request, $id)
+    {
+        $secret="123456";
+
+        $form= $this->createForm(AdminType::class);
+        $form->handleRequest($request);
+
+        $user =$repo->find($id);
+
+        if(!$user){
+            $this->addFlash("error", "Aucun utilisateur trouvé avec l'id $id");
+            return $this->redirectToRoute("app_home");
+        }
+        if($form->isSubmitted() && $form->isValid())
+        {
+            if ($form->get('secret')->getData() == $secret)
+            {
+                $user->setRoles(["ROLE_ADMIN"]);
+            } else {
+                $this->addFlash("error", "Vous n'êtes pas autorisés à effectuer cette action, veuillez contacter l'administrateur du site");
+                return $this->redirectToRoute("app_home");
+            }
+            $repo->add($user,1);
+            $this->addFlash("success", "Vous êtes désormais admin, veuillez vous reconnecter pour profiter de vos privilèges");
+            return $this->redirectToRoute("app_home");
+        }
+        return $this->render("security/passer_admin.html.twig", [
+            "user" => $user,
+            "formAdmin" => $form->createView()
+        ]);
     }
 }
