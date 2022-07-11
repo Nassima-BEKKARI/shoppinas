44eed459157896aaeed91092b0d848e43eaf3e64
nasalia <n.bekkari1987@gmail.com>
2022-07-11 17:20:58 +0200

    modifs layout

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -28,9 +28,10 @@ class ProduitController extends AbstractController
 
     public function allProduits(ProduitRepository $repo, CategorieRepository $repoCat)
     {
+
         $produits = $repo->findAll();
         $categories = $repoCat->findAll();
-
+        
         return $this->render('produit/produits.html.twig', [
             'produits'=>$produits,
             'categories'=>$categories
@@ -42,7 +43,6 @@ class ProduitController extends AbstractController
         //On récupère la catégorie sur laquelle on a cliqué pour accéder aux produits liés
         $categorie = $repo->find($id);
         $produits = $categorie->getProduits();
-
         //On récupère toutes les catégories pour les afficher dans la liste de la page
         $categories = $repo->findAll();
 
